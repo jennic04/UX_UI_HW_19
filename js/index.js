9d27625a7d2e4cc625eac57239f25a876c5bf5be
jennic04 <93343161+jennic04@users.noreply.github.com>
2022-08-03 16:35:05 -0600

    Change to index.js
    Adjusted timing of scroll to top animation

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 500) {
             $("#scroll").fadeIn();
         } else {
             $("#scroll").fadeOut();
         }
     });
+
     $("#scroll").on("click", function () {
         $("html, body").animate({ scrollTop: 0 }, 800);
         return false;
