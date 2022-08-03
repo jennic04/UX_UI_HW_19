5df753a824697ea225697f9f1b4c34bb3975a06e
jennic04 <93343161+jennic04@users.noreply.github.com>
2022-08-03 16:07:59 -0600

    Initial HW19 Commit
    Added index.js with UI interaction on DOI case study page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 100) {
+            $("#scroll").fadeIn();
+        } else {
+            $("#scroll").fadeOut();
+        }
+    });
+    $("#scroll").on("click", function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
